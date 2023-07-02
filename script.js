707f24aefb5b9a3ab6b692af5e954d92914cc88a
Nicholas chuks <niccobery91@gmail.com>
2023-07-03 00:17:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const MenuBtn = document.querySelector("#menu-img");
+// const CloseBtn = document.querySelector("#close-img");
+// const NavList = document.querySelector("#nav-list");
+
+const MenuBtn = document.getElementById("menu-img");
+const CloseBtn = document.getElementById("close-img");
+const NavList = document.getElementById("nav-list");
+
+MenuBtn.addEventListener("click", () => {
+  NavList.classList.toggle("show-sidebar");
+});
+
+// CloseBtn.addEventListener("click", () => {
+//   NavList.classList.remove("show-sidebar");
+//   CloseBtn.classList.remove("show-sidebar");
+// });
